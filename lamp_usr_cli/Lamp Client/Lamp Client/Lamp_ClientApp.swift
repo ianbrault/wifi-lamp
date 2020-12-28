206d4425ab387a8ab0b8d2fc61535c6d1fde8b34
Ian Brault <ian@brault.dev>
2020-12-27 17:02:36 -0800

    Initial skeleton of iOS user client

---
+++ lamp_usr_cli/Lamp Client/Lamp Client/Lamp_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lamp_ClientApp.swift
+//  Lamp Client
+//
+//  Created by Ian Brault on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct Lamp_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
