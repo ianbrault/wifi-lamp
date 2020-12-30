608253c417f67685d740d563cbfc340f71f6385a
Ian Brault <ian@brault.dev>
2020-12-30 14:36:18 -0800

    Add an initial screen to select the lamp owner

--- lamp_usr_cli/Lamp Client/Lamp Client/Lamp_ClientApp.swift
+++ lamp_usr_cli/Lamp Client/Lamp Client/Lamp_ClientApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct Lamp_ClientApp: App {
+    @State private var lampUser = UserDefaults.standard.string(forKey: "lampUser")
+
+    func setLampUser(_ name: String) {
+        UserDefaults.standard.set(name, forKey: "lampUser")
+        lampUser = name
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // if the user has not been set, show the selection view
+            if lampUser == nil {
+                UserSelectView(setLampUser: setLampUser)
+            } else {
+                ContentView()
+            }
         }
     }
 }
---
+++ lamp_usr_cli/Lamp Client/Lamp Client/UserSelectView.swift
@@ -0,0 +1,79 @@
+//
+//  UserSelectView.swift
+//  Lamp Client
+//
+//  Created by Ian Brault on 12/29/20.
+//
+
+import SwiftUI
+
+enum User: String, CaseIterable, Identifiable {
+    case arni
+    case ian
+
+    var id: String { self.rawValue }
+}
+
+struct UserSelectView: View {
+    let setLampUser: (String) -> Void
+    @State private var selectedUser = User.arni
+
+    func onButtonPress() {
+        setLampUser(selectedUser.rawValue.capitalized)
+    }
+
+    var body: some View {
+        ZStack {
+            // color should fill the entire screen
+            Color(.sRGB, red: 73 / 255, green: 89 / 255, blue: 154 / 255)
+                .ignoresSafeArea()
+
+            VStack {
+                Image("rpi_bare_white")
+                    .resizable()
+                    .interpolation(.high)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 32, height: 132)
+
+                Spacer()
+                    .frame(height: 32)
+
+                Text("Welcome!")
+                    .font(.system(size: 32))
+                    .bold()
+                    .italic()
+                    .foregroundColor(Color.white)
+
+                Spacer()
+                    .frame(height: 4)
+
+                Text("Whose lamp is this?")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color.white)
+
+                Picker("User", selection: $selectedUser) {
+                    ForEach(User.allCases) { user in
+                        Text(user.rawValue.capitalized)
+                            .foregroundColor(Color.white)
+                    }
+                }
+
+                Spacer()
+                    .frame(height: 64)
+
+                Button(action: onButtonPress) {
+                    Text("Continue")
+                        .font(.system(size: 20))
+                        .bold()
+                        .foregroundColor(Color.white)
+                }
+            }
+        }
+    }
+}
+
+struct UserSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserSelectView(setLampUser: {s in return})
+    }
+}
